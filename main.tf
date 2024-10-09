656b16c56ae6467705897581f8ce7b8b85863012
waddamski <andy.waddams@digital.hmrc.gov.uk>
2024-10-09 17:21:29 +0100

    PSEC-2211 - conditionally apply acl

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle_configuration
 
 resource "aws_s3_bucket_acl" "bucket_acl_private" {
   depends_on = [aws_s3_bucket_ownership_controls.bucket_owner_enforced]
-
+  count = var.bucket_object_ownership != "BucketOwnerEnforced" ? 1 : 0
   bucket = aws_s3_bucket.bucket.id
   acl    = "private"
 }
