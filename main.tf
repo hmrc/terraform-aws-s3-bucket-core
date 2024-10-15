ca622d9d4e7cd284c19faec8fd7117bc81702c9f
waddamski <andy.waddams@digital.hmrc.gov.uk>
2024-10-15 17:29:10 +0100

    PSEC-2211 - fix logic issue for versioning

--- main.tf
+++ main.tf
@@ -91,7 +91,7 @@ resource "aws_s3_bucket_acl" "bucket_acl_private" {
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
-    status = var.versioning_enabled != "true" ? "Disabled" : "Enabled"
+    status = var.versioning_enabled != true ? "Disabled" : "Enabled"
   }
 }
 
